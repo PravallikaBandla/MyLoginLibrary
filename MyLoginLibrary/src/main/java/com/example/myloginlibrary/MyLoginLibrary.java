71d482cd8f0699648bf689fb6dfc9d18127b765d
pravallikab <pravallikab.in@mouritech.com>
2021-03-23 18:04:57 +0530

    Initial Commit done

---
+++ MyLoginLibrary/src/main/java/com/example/myloginlibrary/MyLoginLibrary.java
@@ -0,0 +1,21 @@
+package com.example.myloginlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Pravallika on 23-03-2021.
+ * Updated by Pravallika on 23-03-2021.
+ * <p>
+ * <p>
+ * <p>
+ * Copyright (c) 2021 MouriTech. All rights reserved.
+ */
+public class MyLoginLibrary {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
